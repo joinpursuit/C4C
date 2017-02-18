b809dbf8a3f22f41cc29399bad6dfde27c0b1ee6
Sabrina Ip <kkip90@gmail.com>
2017-02-18 00:53:30 -0500

    geoswift integrated

--- VhsHackcess/CommunityBoardViewController.swift
+++ VhsHackcess/CommunityBoardViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import GEOSwift
+import MapKit
 
 class CommunityBoardViewController: UIViewController {
 
     @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var infoTextView: UITextView!
     
+    var districts = [String]()
+    var geometries: [Geometry]? = [Geometry]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getGeometries()
     }
 
     // MARK: - Actions
@@ -32,5 +38,35 @@ class CommunityBoardViewController: UIViewController {
         return true
     }
     
-
+    
+    
+    func getGeometries() {
+        guard let geoJSONURL = Bundle.main.url(forResource: "Community Districts", withExtension: "geojson"),
+            let jsonData = try? Data(contentsOf: geoJSONURL) else { return }
+        
+        do {
+            let parsedObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
+            guard let parsedObjectCasted = parsedObject as? [String: Any],
+                let features = parsedObjectCasted["features"] as? [[String: Any]] else {
+                    return
+            }
+            
+            for each in features {
+                guard let properties = each["properties"] as? [String:Any] else { continue }
+                if let boroCd = properties["boro_cd"] as? String {
+                    districts.append(boroCd)
+                }
+            }
+        } catch {
+            print(error)
+        }
+        
+        do {
+            let geometries = try Geometry.fromGeoJSON(geoJSONURL)
+            self.geometries = geometries
+        }
+        catch {
+            print(error)
+        }
+    }
 }
--- VhsHackcess/MapViewController.swift
+++ VhsHackcess/MapViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import GEOSwift
 
 class MapViewController: UIViewController {
 
@@ -18,6 +19,7 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addBoundry()
     }
 
     // MARK: - Actions
@@ -27,5 +29,51 @@ class MapViewController: UIViewController {
 
     @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
     }
+    
+    func addBoundry() {
+//        if let geoJSONURL = Bundle.main.url(forResource: "Community Districts", withExtension: "geojson") {
+//            do {
+//                let geometries = try Geometry.fromGeoJSON(geoJSONURL)
+//                if let geo = geometries?[0] as? MultiPolygon {
+//                    
+//                    if let shapesCollection = geo.mapShape() as? MKShapesCollection {
+//                        
+//                        let shapes = shapesCollection.shapes
+//                        
+//                        for shape in shapes {
+//                            if let polygon = shape as? MKPolygon {
+//                                mapView.add(polygon)
+//                            }
+//                        }
+//                    }
+//                    
+//                }
+//            } catch {
+//                print("Unable to load geojson data")
+//            }
+//        }
+    }
+    
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    /*
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+     
+     // Configure the cell...
+     
+     return cell
+     }
+     */
     
 }
