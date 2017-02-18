22e3bfb7d705c10055aac4b3bfe5dcba6848606b
Victor Zhong <viczhong@gmail.com>
2017-02-18 17:49:25 -0500

    Messaging cell nib file

--- VhsHackcess/MessageBoardViewController.swift
+++ VhsHackcess/MessageBoardViewController.swift
@@ -13,13 +13,10 @@ class MessageBoardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    
     
 
     /*
