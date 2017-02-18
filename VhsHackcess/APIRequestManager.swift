328147a90d097950c352a4c376f6eb3359c01ac1
harichandansingh <singh.harichandan@gmail.com>
2017-02-18 10:55:54 -0500

    Created APIRequestManager for API call.

---
+++ VhsHackcess/APIRequestManager.swift
@@ -0,0 +1,33 @@
+//
+//  APIRequestManager.swift
+//  VhsHackcess
+//
+//  Created by Harichandan Singh on 2/18/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import Foundation
+
+class APIRequestManager {
+    //MARK: - Properties
+    static let manager = APIRequestManager()
+    let endpoint: String = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json"
+    
+    //MARK: - Initializer
+    private init() {}
+    
+    //MARK: - Methods
+    func getData(endPoint: String = self.endpoint, callback: @escaping (Data?) -> Void) {
+        guard let myURL = URL(string: endPoint) else { return }
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        
+        session.dataTask(with: myURL) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error != nil {
+                print("Error durring session: \(error)")
+            }
+            guard let validData = data else { return }
+            
+            callback(validData)
+            }.resume()
+    }
+}
