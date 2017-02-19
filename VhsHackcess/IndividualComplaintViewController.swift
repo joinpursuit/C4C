c6fefc4f12db0f6d65dd056f4f8e7ff6eba026ec
Sabrina Ip <kkip90@gmail.com>
2017-02-19 13:51:42 -0500

    refactor request, individualComplaintVC has correct label

--- VhsHackcess/IndividualComplaintViewController.swift
+++ VhsHackcess/IndividualComplaintViewController.swift
@@ -10,10 +10,59 @@ import UIKit
 
 class IndividualComplaintViewController: UIViewController {
 
-    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var infoTextView: UITextView!
+    var request: ServiceRequest?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupInfoLabel()
     }
+ 
+    func setupInfoLabel() {
+        guard let request = request else { return }
+        var label = "Complaint Type: \(request.complaintType)"
+        
+        if let description = request.descriptor {
+            label += "\nDescription: \(description)"
+        }
+        
+        label += "\nDate: \(request.createdDate)\nStatus: \(request.status)"
+        
+        if let resolution = request.resolutionDescription {
+            label += "\nResolution: \(resolution)"
+        }
+        
+        label += "\nCommunity Board: \(request.communityBoard)"
+        
+        if let locType = request.locationType {
+            label += "\nLocation Type: \(locType)"
+        }
+ 
+        if let incidentAddress = request.incidentAddress {
+            label += "\nIncident Address: \(incidentAddress)"
+        }
+        
+        if let crossStreet1 = request.crossStreet1,
+            let crossStreet2 = request.crossStreet2 {
+            label += "\nCross Streets: \(crossStreet1) & \(crossStreet2)"
+        }
+
+        
+        if let bridgeHighwayName = request.bridgeHighwayName {
+            label += "\nBridge/Highway: \(bridgeHighwayName)"
+        }
+        
+        if let bridgeHighwaySegment = request.bridgeHighwaySegment {
+            label += "\nSegment: \(bridgeHighwaySegment)"
+        }
+        
+        if let bridgeHighwayDirection = request.bridgeHighwaySegment {
+            label += "\nDirection: \(bridgeHighwayDirection)"
+        }
+        
+        infoTextView.text = label
+    }
+
+ 
 
 }
