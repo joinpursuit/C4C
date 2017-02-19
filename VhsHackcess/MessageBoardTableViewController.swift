d01916c688bc8330dccd6e4b4123ce75f183d026
Victor Zhong <viczhong@gmail.com>
2017-02-19 10:00:50 -0500

    Committing before pull

--- VhsHackcess/MessageBoardTableViewController.swift
+++ VhsHackcess/MessageBoardTableViewController.swift
@@ -54,10 +54,10 @@ class MessageBoardTableViewController: UITableViewController {
                     if let snap = child as? FIRDataSnapshot,
                         let valueDict = snap.value as? [String : Any] {
                         let post = Post(uid: valueDict["uid"] as! String, author: valueDict["author"] as! String, title: valueDict["title"] as! String, body: valueDict["body"] as! String, commentCount: valueDict["commentCount"] as! Int)
-                        
                         self.posts.append(post)
                     }
                 }
+                
                 self.tableView.reloadData()
             })
         }
@@ -84,42 +84,6 @@ class MessageBoardTableViewController: UITableViewController {
         return cell
     }
     
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
     /*
      // MARK: - Navigation
      
@@ -129,5 +93,5 @@ class MessageBoardTableViewController: UITableViewController {
      // Pass the selected object to the new view controller.
      }
      */
-    
+        
 }
