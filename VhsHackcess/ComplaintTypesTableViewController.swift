ea058fdf126555ad80ce0f3865d0f01adc402686
harichandansingh <singh.harichandan@gmail.com>
2017-02-19 10:13:06 -0500

    TableView populating with sorted data

--- VhsHackcess/ComplaintTypesTableViewController.swift
+++ VhsHackcess/ComplaintTypesTableViewController.swift
@@ -26,6 +26,9 @@ class ComplaintTypesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = self.communityBoard
+        
+        //populate complaintTypes dictionary
         APIRequestManager.manager.getData(endPoint: self.openDataEndpoint) { (data: Data?) in
             
             if let cbRequests = ServiceRequest.getServiceRequests(data: data!) {
@@ -67,59 +70,22 @@ class ComplaintTypesTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
-        let complaint = self.tuples[indexPath.row]
+        
+        //sorted results
+        let complaint = self.tuples.sorted { $0.1 > $1.1 }[indexPath.row]
         
         cell.textLabel?.text = "\(complaint.0) (\(complaint.1))"
         return cell
     }
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
+
+     //MARK: - Navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
+        if let dvc = segue.destination as? MapViewController {
+            //pass in endpoint parameters
+        }
      }
-     */
     
     //MARK: - Actions
-    
     @IBAction func filterTapped(_ sender: UIBarButtonItem) {
     }
     
